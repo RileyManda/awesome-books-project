a028c6dc7e7271763a50b92a04a18a16997fd781
RileyManda <rileymanda69@gmail.com>
2023-06-12 16:49:43 +0200

    Updated time-date format

--- index.js
+++ index.js
@@ -1,33 +1,11 @@
-import { DateTime } from './node_modules/luxon/src/luxon.js';
+// import { DateTime } from './node_modules/luxon/src/luxon.js';
+import updateDateTime from './modules/displayTime.js';
 import BookCollection from './modules/bookCollection.js';
 import initializeMenu from './modules/menu.js';
 import initializeForm from './modules/form.js';
 
 // Function for displaying date and time
 const dateTimeElement = document.querySelector('#datetime');
-
-function updateDateTime() {
-  const currentDateTime = DateTime.now()
-    .setZone('America/New_York')
-    .minus({ weeks: 1 })
-    .endOf('day');
-
-  const formattedDateTime = currentDateTime.toLocaleString({
-    weekday: 'short',
-    year: 'numeric',
-    month: 'short',
-    day: '2-digit',
-    hour: '2-digit',
-    minute: '2-digit',
-    second: '2-digit',
-    hour12: false,
-  });
-
-  // Update the text content of the DOM element
-  dateTimeElement.textContent = formattedDateTime;
-}
-
-updateDateTime();
 // book collection
 const collection = new BookCollection();
 // save data
@@ -35,3 +13,4 @@ collection.saveData();
 // dom elements:Menu and Form
 initializeMenu(collection);
 initializeForm(collection);
+updateDateTime(dateTimeElement);
--- modules/displayTime.js
+++ modules/displayTime.js
@@ -1,13 +1,24 @@
-// import { DateTime } from '../node_modules/luxon/src/luxon.js';
+import { DateTime } from '../node_modules/luxon/src/luxon.js';
 // import { DateTime } from 'luxon';
 
-// export function displayDateTime() {
-//   const formattedDateTime = DateTime.now()
-//     .setZone('America/New_York')
-//     .minus({ weeks: 1 })
-//     .endOf('day')
-//     .toISO();
+// Function for updating and displaying date and time
+const updateDateTime = (dateTimeElement) => {
+  const currentDateTime = DateTime.now()
+    .setZone('Europe/Paris')
+    .minus({ weeks: 1 })
+    .endOf('day');
 
-//   console.log(formattedDateTime);
-// }
-// export default displayDateTime();
+  const formattedDateTime = currentDateTime.toLocaleString({
+    month: 'short',
+    day: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+  });
+
+  // Update the text content of the DOM element
+  dateTimeElement.textContent = formattedDateTime;
+};
+
+export default updateDateTime;

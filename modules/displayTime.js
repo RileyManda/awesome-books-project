adf68759dc4c9abd555efb69a20beed890686d57
RileyManda <rileymanda69@gmail.com>
2023-06-12 16:53:47 +0200

    Implemented set interval for live updates

--- modules/displayTime.js
+++ modules/displayTime.js
@@ -1,24 +1,29 @@
 import { DateTime } from '../node_modules/luxon/src/luxon.js';
-// import { DateTime } from 'luxon';
+// eslint-disable-next-line quotes
+// import { DateTime } from "/luxon";
 
 // Function for updating and displaying date and time
 const updateDateTime = (dateTimeElement) => {
-  const currentDateTime = DateTime.now()
-    .setZone('Europe/Paris')
-    .minus({ weeks: 1 })
-    .endOf('day');
+  const updateClock = () => {
+    const currentDateTime = DateTime.now().setZone('Europe/Paris');
+    const formattedDateTime = currentDateTime.toLocaleString({
+      month: 'short',
+      day: '2-digit',
+      year: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+    });
 
-  const formattedDateTime = currentDateTime.toLocaleString({
-    month: 'short',
-    day: '2-digit',
-    year: 'numeric',
-    hour: '2-digit',
-    minute: '2-digit',
-    second: '2-digit',
-  });
+    // Update the text content of the DOM element
+    dateTimeElement.textContent = formattedDateTime;
+  };
 
-  // Update the text content of the DOM element
-  dateTimeElement.textContent = formattedDateTime;
+  // Update the clock immediately
+  updateClock();
+
+  // Update the clock every second
+  setInterval(updateClock, 1000);
 };
 
 export default updateDateTime;
